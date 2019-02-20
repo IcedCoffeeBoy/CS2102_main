719dd3ade1c67e49ecd7d28094928415be04b945
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-21 02:24:31 +0800

    Formated the ejs file

--- public/javascripts/indexScript.js
+++ public/javascripts/indexScript.js
@@ -1,17 +1,5 @@
-var db = require('../../db')
+$('.message').click(function () {
+    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
 
-function connectDB(){
-    db.connect(function (err) {
-        if (err) {
-           console.log(err)
-           alert("unable to connect")
-        }
-        else {
-            console.log("Sucessfully connected to database");
-            alert("successfully connected!")
-        }
-    });
 
-}
-
-module.exports = connectDB
