517917642c9ffdb64e82d113b9e23adc01908a74
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-20 19:05:56 +0800

    testing online database

--- db.js
+++ db.js
@@ -6,18 +6,19 @@ Run start postreqsql
 Ensure that the correct user, host, database, password, port is used  
 */
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    //password: '********', My postregsql doesn't use any password
-    port: 5432,
+    user: 'ckdtnpmamiywyi',
+    host: 'ec2-54-204-2-25.compute-1.amazonaws.com',
+    database: 'dd15caf96d7vac',
+    password: '7eb2c774f7473c16e33e941002cbfcecfa94845782aaeb770001f79459e25cee'
 });
 
 pool.connect(function (err) {
     if (err) {
        console.log(err)
+       alert("Unable to connect to database")
     }
     else {
+        alert("Sucessfully connected to database")
         console.log("Sucessfully connected to database");
     }
 });
