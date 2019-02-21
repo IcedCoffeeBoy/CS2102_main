31adf1484179ad5b989ab641aa29b43301342695
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-21 17:16:30 +0800

    alert for register

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -47,6 +48,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -21,9 +21,9 @@ router.get('/login', function (req, res, next) {
       console.log(err);
     } else {
       if (data.rowCount == 1) {
-        res.redirect('/insert');
+        res.redirect('/main');
       } else {
-        res.sendStatus(404);
+        res.send(500,'Unable to find user') 
       }
     }
   })
@@ -34,19 +34,19 @@ router.post('/reg', function (req, res, next) {
   var password = req.body.password;
   var email = req.body.email;
 
-  console.log(req.body);
-  console.log(req.query);
+  console.log(req.body)
 
   var sqlquery = "insert into accounts(username,password,email) values($1,$2,$3)";
 
   db.query(sqlquery, [username, password, email], function (err, data) {
       if(err){
         console.log(err)
+        res.send({valid: false})
       } else {
+        console.log("Sucessfully created account")
         res.redirect('/')
       }
   })
-
 })
 
 module.exports = router;
