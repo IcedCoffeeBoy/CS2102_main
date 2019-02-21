8b03adced96672179312d0d6f936d596a86a5d9d
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-21 11:15:21 +0800

    added sql script

---
+++ database.sql
@@ -0,0 +1,17 @@
+create sequence accountsid_seq start 1000; 
+
+create table accounts (
+	accountid integer NOT NULL DEFAULT nextval('accountsid_seq') primary key,
+	username varchar(16) not null,
+	password varchar(16) not null,
+	email varchar(30) not null unique
+);
+
+ALTER SEQUENCE accountsid_seq OWNED BY accounts.accountid;
+
+
+
+insert into accounts(username,password,email) values('1234','1234','dog@gmail.com');
+insert into accounts(username,password,email) values('perry','wang','perrywang@gmail.com');
+
+ 
