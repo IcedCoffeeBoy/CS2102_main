c87f4dc3aefcfd7a03b6395477ba904a400bac54
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-22 17:34:16 +0800

    changed the main page

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var db = require("../db")
 
-router.get('/', function(req, res, next) {
-  res.render('main', { title: 'main' });
+router.get('/', function (req, res, next) {
+
+  res.render('main', {
+    title: 'main', data: [
+      {title:'Good doggo', desc: "Dogs for sharing", price: "99", img:"https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg?quality=98&strip=all" },
+      {title:'Cute cats', desc: "Cats for you to serve", price: "21", img: "https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=978&q=80"  },
+    ]
+  });
 });
 
 
