439ea28c5630ae1f038bcb06483085a63015c552
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-23 14:02:53 +0800

    pulling data from sql

--- database.sql
+++ database.sql
@@ -14,4 +14,14 @@ insert into accounts(username,password,email) values('1234','1234','dog@gmail.co
 insert into accounts(username,password,email) values('perry','wang','perrywang@gmail.com');
 
 
+create table items(
+	itemid serial primary key,
+	title varchar(80),
+	description text, 
+	price integer, 
+	img varchar(200),
+);
+
+insert into items(title,description,price,img) values ('Good doggo','Dogs for sharing','99','https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg?quality=98&strip=all');
+insert into items(title,description,price,img) values ('Cute cats', 'Cats for you to serve', '21', 'https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=978&q=80');
 
--- routes/main.js
+++ routes/main.js
@@ -3,14 +3,20 @@ var router = express.Router();
 var db = require("../db")
 
 router.get('/', function (req, res, next) {
+  var sqlquery = 'select * from items'
+  var sampledata = [
+    { title: 'Good doggo', description: "Dogs for sharing", price: "99", img: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg?quality=98&strip=all" },
+    { title: 'Cute cats', description: "Cats for you to serve", price: "21", img: "https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=978&q=80" },
+  ]
 
-  res.render('main', {
-    title: 'main', data: [
-      {title:'Good doggo', desc: "Dogs for sharing", price: "99", img:"https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg?quality=98&strip=all" },
-      {title:'Cute cats', desc: "Cats for you to serve", price: "21", img: "https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=978&q=80"  },
-    ]
-  });
+  db.query(sqlquery, function (err, data) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(data.row)
+      res.render('main', { title: 'main', data: data.rows });
+    }
+  })
 });
-
 
 module.exports = router;
