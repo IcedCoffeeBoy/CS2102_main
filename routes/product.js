0fb4a70acf9226e000bf219b1b685a16be96668c
perrythewang <perry.wzm@gmail.com>
2019-03-09 00:39:17 +0800

    Changed asynchronous code to use JS promises

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,19 @@ var express = require("express");
 var router = express.Router();
 var db = require("../db");
 
+function db_query(sql, args) {
+  return new Promise((resolve, reject) => {
+    db.query(sql, args, (err, data) => {
+      if (err) {
+        console.log("SQL error:" + err);
+        return reject(err);
+      } else {
+        resolve(data.rows);
+      }
+    })
+  });
+}
+
 router.get("/", function(req, res, next) {
   res.send("<h1>No product listing code provided!</h1>");
 });
@@ -10,37 +23,37 @@ router.get("/:productId", function(req, res, next) {
   var mainquery =
     "select title, description, price, username from items natural join accounts where itemid = $1";
   var imgquery = "select imgurl from images where itemid = $1";
+  var revquery = "select review from reviews where itemid = $1";
   var sidequery =
     "select itemid, title, description, price, imgurl from items natural join images";
-  var reviewquery = "select review from "
+  
   var itemid = req.params.productId;
   var data = {};
-  db.query(mainquery, [itemid], function(err, maindata) {
-    if (err) {
-      console.log("SQL error:" + err);
-    } else {
-      db.query(imgquery, [itemid], (err, mainimg) => {
-        if (err) {
-          console.log("SQL error:" + err);
-        } else {
-          db.query(sidequery, (err, mainrecs) => {
-            if (err) {
-              console.log(err);
-            } else {
-              res.render("product", {
-                title: "productlisting",
-                data: maindata.rows[0],
-                imgs: mainimg.rows,
-                recs: mainrecs.rows,
-                user: req.user,
-                tparam: req.params.productId
-              });
-            }
-          });
-        }
+  db_query(mainquery, [itemid])
+    .then((rows) => {
+      data.main = rows[0];
+      return db_query(imgquery, [itemid])
+    })
+    .then((rows) => {
+      data.imgs = rows;
+      return db_query(revquery, [itemid])
+    })
+    .then((rows) => {
+      data.reviews = rows;
+      return db_query(sidequery)
+    })
+    .then((rows) => {
+      data.recomms = rows;
+      res.render("product", {
+        title: "productlisting",
+        data: data.main,
+        imgs: data.imgs,
+        revs: data.reviews,
+        recs: data.recomms,
+        user: req.user
       });
-    }
-  });
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
