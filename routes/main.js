333fc34568929f2ac412c80bc0d5a59635e99ae9
perrythewang <perry.wzm@gmail.com>
2019-03-13 14:41:21 +0800

    Fixed product recommendations display

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require("../db");
 
 router.get('/', function (req, res, next) {
-  var sqlquery = 'select itemid,title, description, price, imgurl from items natural join images'
+  var sqlquery = 'select itemid,title, description, price, imgurl from items natural join images where imgno = 0'
   
   db.query(sqlquery, function (err, data) {
     if (err) {
--- routes/product.js
+++ routes/product.js
@@ -21,17 +21,19 @@ router.get("/", function(req, res, next) {
 
 router.get("/:productId", function(req, res, next) {
   var mainquery =
-    "select title, description, price, username from items natural join accounts where itemid = $1";
+    "select title, description, price, username, catname from items join accounts on items.seller = accounts.accountid where itemid = $1";
   var imgquery = "select imgurl from images where itemid = $1";
   var revquery = "select review from reviews where itemid = $1";
   var sidequery =
-    "select itemid, title, description, price, imgurl from items natural join images";
+    "select itemid, title, description, price, imgurl from items natural join images where imgno=0 limit 4";
   
   var itemid = req.params.productId;
   var data = {};
+  console.log()
   db_query(mainquery, [itemid])
     .then((rows) => {
       data.main = rows[0];
+      console.log("test" + data.main);
       return db_query(imgquery, [itemid])
     })
     .then((rows) => {
