75a7e8a13206c21e697d33a686c9a109232ebb8f
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-04-01 16:44:46 +0800

    block bid for non-login user

--- routes/user.js
+++ routes/user.js
@@ -2,31 +2,20 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 var chatRouter = require('./chat');
-
-var sql_getItems =
-  'SELECT * ' +
-  'FROM Items NATURAL JOIN Images ' +
-  'WHERE seller = $1 AND imgno = 0' +
-  'ORDER BY timeListed DESC';
-
-const sql_getDatejoined = 'select datejoined from accounts where accountid = $1';
-const sql_getBidItems = "select itemid, description, max(amount) as amount, title, price, imgurl " +
-  "from relationships natural join bids natural join items natural join images " +
-  "where imgNo=0 and buyer=$1 " + 
-  "group by itemid, description, title, price, imgurl"
+const sql = require('../sql/index');
 
 /* GET user listing. */
 router.get('/', async (req, res, next) => {
   try {
     // Attempt parallel SQL execution for speed
     let results = await Promise.all([
-      db.db_promise(sql_getDatejoined, [req.user.id]),
-      db.db_promise(sql_getItems, [req.user.id]),
-      db.db_promise(sql_getBidItems, [req.user.id])
-    ])
-    let options = { year: 'numeric', month: 'long', day: 'numeric' }
-    let datejoineds = results[0]
-    datejoined = datejoineds[0].datejoined.toLocaleDateString("en-US", options)
+      db.db_promise(sql.sql_getDatejoined, [req.user.id]),
+      db.db_promise(sql.sql_getItems, [req.user.id]),
+      db.db_promise(sql.sql_getBidItems, [req.user.id])
+    ]);
+    let options = { year: 'numeric', month: 'long', day: 'numeric' };
+    let datejoineds = results[0];
+    datejoined = datejoineds[0].datejoined.toLocaleDateString("en-US", options);
 
     let data = results[1]
     let biditems = results[2]
--- sql/index.js
+++ sql/index.js
@@ -1,10 +1,14 @@
-const sql = {}
-
-sql.query = {
+var sql = {
     // Search
     search_items: 'select title, description, price, imgurl,itemid from items natural join images where lower(title) like $1 and imgno = 0',
     search_categories: 'select catname from categories where lower(catname) like $1',
     search_users: 'select username,accountid from accounts where lower(username) like $1',
+    sql_getItems: 'SELECT * FROM Items NATURAL JOIN Images WHERE seller = $1 AND imgno = 0 ORDER BY timeListed DESC',
+    sql_getuserinfo: 'select datejoined, username from accounts where accountid = $1',
+    sql_getDatejoined: 'select datejoined from accounts where accountid = $1',
+    sql_getBidItems: "select itemid, description, max(amount) as amount, title, price, imgurl " +
+        "from relationships natural join bids natural join items natural join images " +
+        "where imgNo=0 and buyer=$1 group by itemid, description, title, price, imgurl"
 }
 
 module.exports = sql;
