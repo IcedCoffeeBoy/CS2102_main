671d598393f510f1fd53418bd10f60212d24e39c
Perry Wang <perry.wzm@gmail.com>
2019-04-08 01:01:06 +0800

    Added Google application credentials JSON builder

---
+++ keyfileBuilder.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+
+makeKeyfileJson = (keyfilePath) => {
+  // Begin building only if no existing keyfile is found
+  fs.access(keyfilePath, fs.F_OK, (err) => {
+    if (err) {
+      var encoded = process.env.ENCODED_GOOGLE_CREDENTIALS
+      if (!encoded) {
+          console.log("Warning: Could not find credentials environment variable!")
+          return
+      }
+      var decoded = Buffer.from(encoded, 'base64').toString('binary');
+      fs.writeFileSync(keyfilePath, decoded);
+      return
+    }
+  })
+}
+
+module.exports = makeKeyfileJson;
