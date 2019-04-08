422c8d0587a12357481151a597f8709744e64f38
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-04-08 15:26:26 +0800

    edit main.py

--- datagenerator/main.py
+++ datagenerator/main.py
@@ -4,8 +4,14 @@ from dbpopulator import db_populate
 
 
 if __name__=="__main__":
+    #Set HEROKU_SERVER to True to edit heroku postgresql
+    HEROKU_SERVER = False
+
     load_dotenv(verbose=True)
     DATABASE_URL = os.getenv('DATABASE_URL')
     path = "../database.sql"
-    db_populate(10, script_path=path)
+    if(HEROKU_SERVER):
+        db_populate(10, script_path=path, url=DATABASE_URL)
+    else:
+        db_populate(10, script_path=path)
 
