5e0e68b493b359332d87042da6eae11e29139f85
qinjiang <qinjiang03@gmail.com>
2019-04-10 04:54:16 +0800

    added editlisting. need help with button layout

--- sql/index.js
+++ sql/index.js
@@ -119,7 +119,11 @@ var sql = {
                           )
                         )
                         ORDER BY likes desc`,
-  
+    
+    // Edit Listing
+    sql_getItemInfo: 'SELECT * FROM Items WHERE itemid = $1',
+    sql_updateItem: "UPDATE Items SET title=$1, description=$2, price=$3, catname=$4, loanstart=$5, loanend=$6, location=$7 WHERE itemid=$8 RETURNING itemid",
+    
 }
 
 module.exports = sql;
