035307a7da1be61dd09131b32cd70838733c617d
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-04-10 14:58:14 +0800

    fixed bugs

--- routes/admin.js
+++ routes/admin.js
@@ -8,15 +8,15 @@ router.get('/', async function (req, res, next) {
     const sql_getallitems = 'select * from items';
     const sql_getallusers = "select * from accounts"
     let userid = req.user.id;
-    
+
     //Check if user is admin
     if (!db.db_checkadmin(userid)) {
         return res.sendStatus(403);
     }
     try {
         let item = await db.db_promise(sql_getallitems);
-        let user = await db.db_promise(sql_getallusers);
-        res.render('admin', { user: req.user, item: item, user: req.user });
+        let users = await db.db_promise(sql_getallusers);
+        res.render('admin', { user: req.user, item: item, users: users });
     } catch (err) {
         console.log(err)
         res.sendStatus(500);
